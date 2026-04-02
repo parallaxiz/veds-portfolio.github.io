6208c70be7545a1637802457745e22122b6052ac
VedMadurwar <ved.madurwar24@vit.edu>
2026-04-02 09:43:50 +0530

    reccruters issue solved

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        recruiters: resolve(__dirname, 'recruiters.html'),
+      },
+    },
   },
-  base: process.env.NODE_ENV === 'production'
-    ? '/' // For Vercel deployment
-    : '/',
-});
+})
