093a805c2edc4e5c97cba609ced8d123f8d98207
VedMadurwar <ved.madurwar24@vit.edu>
2026-07-31 20:27:10 +0530

    Updates

--- assets/recruiters-CJGEGCqC.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e,r,R as o}from"./RecruitersView-KoPjghQL.js";t.createRoot(document.getElementById("root")).render(e.jsx(r.StrictMode,{children:e.jsx(o,{})}));
---
+++ assets/recruiters-C_ow5WqB.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,r,R as o}from"./RecruitersView-CrcMoz9M.js";t.createRoot(document.getElementById("root")).render(e.jsx(r.StrictMode,{children:e.jsx(o,{})}));
